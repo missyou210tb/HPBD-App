94dc45bda594820301ec7f99ba3813cc42a89a62
Luong <militooke@gmail.com>
2020-12-09 10:33:16 +0700

    complete

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -6,15 +6,25 @@ class AdminController < ApplicationController
       format.html
       format.js
     end
-end
+  end
 
   def edit
       @user = User.find_by(id: params[:id])
   end
+
   def create_user
     @user = User.new()
   end
+
   def post_user
+    user = User.find_by(nickname: params[:user][:nickname])
+    if user.present?
+      @user = User.new
+      flash.now[:errors] = "Nickname is exists"
+      render action: :create_user
+      return
+    end
+
     @user = User.new(permitted_params)
     if @user.save
         flash[:success] = "Register success"
@@ -24,9 +34,17 @@ end
         @user = User.new()
         render :create_user
     end
+
   end
 
   def update
+    user = User.find_by(nickname: params[:user][:nickname])
+    if user.present?
+      @user = User.new
+      flash.now[:errors] = "Nickname is exists"
+      render action: :create_user
+      return
+    end
       @user = User.find_by(id: params[:id])
       if @user.update(permitted_params)
           flash[:success] = "Update success"
@@ -36,13 +54,16 @@ end
           render :edit
       end
   end
+
   def destroy
       @user = User.find_by(id: params[:id])
       @user.destroy
       redirect_to "/admin"
   end
+
   private 
   def permitted_params
       params.require(:user).permit(:name,:nickname,:birthday)
   end
+
 end
--- app/jobs/send_job.rb
+++ app/jobs/send_job.rb
@@ -1,64 +1,74 @@
 require 'slack-ruby-client'
 class SendJob < ApplicationJob
+
   queue_as :default 
+
   def perform
-    usersdataall = User.all
-    usersdata = []
-    usersdataall.each do |userdataall|
-      if ((birthdaythisyear(userdataall.birthday) - Time.zone.now.to_date) <=7) &&  ((birthdaythisyear(userdataall.birthday) - Time.zone.now.to_date) > 0)
-        usersdata.push(userdataall)
+    users_data_all = User.all
+    users_data = []
+    users_data_all.each do |user_data_all|
+      if ((birthdaythisyear(user_data_all.birthday) - Time.zone.now.to_date) <=7) &&  ((birthdaythisyear(user_data_all.birthday) - Time.zone.now.to_date) > 0)
+        users_data.push(user_data_all)
       end
     end
     client = Slack::Web::Client.new
     client.auth_test
-    if usersdata.first.present?
-      tagnames = []
-      usersdata.each do |userdata|
+    users_client = client.users_list['members']
+    if users_data.first.present?
+      tag_names = []
+      users_data.each do |user_data|
         temp = 0
-        client.users_list['members'].each do |userclient|
+        users_client.each do |user_client|
           temp = temp + 1
-          if get_nickname_from_display_name(userclient['profile']['display_name']) == userdata.nickname
-            text ='<@' + userclient['id'] + '|cal> '
-            tagnames.push(text)
+          if get_nickname_from_display_name(user_client['profile']['display_name']) == user_data.nickname
+            text ='<@' + user_client['id'] + '|cal> '
+            tag_names.push(text)
             break
           end
         end
-        if temp = client.users_list['members'].size
-          text = userdata.name + ' (' + userdata.nickname + ')'
-          tagnames.push(text)
+        if temp == users_client.size
+          text = user_data.name + ' (' + user_data.nickname + ')'
+          tag_names.push(text)
         end
       end
-      string = ''
-      temp1 = 0
-      tagnames.each do |tagname|
-        temp1 = temp1 + 1
-        if temp1 == tagnames.size
-          string = string + tagname
-        else
-          string = string + tagname + ','
-        end 
-      end
-      messagesend = I18n.t('upcoming',tagnames: string) +  I18n.t('link')
-      client.chat_postMessage(channel: 'gmv-birthday-bot',text: messagesend,as_user: true)
+      string = get_string_tag_name(tag_names)
+      message_send = I18n.t('upcoming',tagnames: string) +  I18n.t('link')
+      client.chat_postMessage(channel: 'gmv-birthday-bot',text: message_send,as_user: true)
     end
   end
   def birthdaythisyear birthday
-    daymonth = birthday.to_s.slice(4...birthday.to_s.size)
-    birthday = Date.parse(Time.zone.now.year.to_s + daymonth) 
+    day_month = birthday.to_s.slice(4...birthday.to_s.size)
+    birthday = Date.parse(Time.zone.now.year.to_s + day_month) 
     return birthday
   end 
+
   def get_nickname_from_display_name display_name
-    stringtemp = display_name.split(" ")
-    stringtemp = stringtemp.join("")
-    if stringtemp.index("(")
-      if stringtemp.index(")")
-        a = stringtemp.index("(") + 1
-        b = stringtemp.index(")") - 1
-        return stringtemp[a..b]
+    string_temp = display_name.split(" ")
+    string_temp = string_temp.join("")
+    if string_temp.index("(")
+      if string_temp.index(")")
+        a = string_temp.index("(") + 1
+        b = string_temp.index(")") - 1
+        return string_temp[a..b]
+      else
+        return nil
+      end
     else
       return nil
     end
-    else return nil
+  end
+
+  def get_string_tag_name tag_names
+    string = ''
+    temp1 = 0
+    tag_names.each do |tag_name|
+      temp1 = temp1 + 1
+      if temp1 == tag_names.size
+        string = string + tag_name
+      else
+        string = string + tag_name + ','
+      end 
     end
+    return string
   end
 end
